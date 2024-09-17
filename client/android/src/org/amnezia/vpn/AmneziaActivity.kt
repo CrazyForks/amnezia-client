27924e996a1255f5ba3cf1a72ed82128d1df01cc
albexk <albexk@proton.me>
2024-09-17 20:52:31 +0300

    Add a method to detect Google Play build

--- client/android/src/org/amnezia/vpn/AmneziaActivity.kt
+++ client/android/src/org/amnezia/vpn/AmneziaActivity.kt
@@ -1177,6 +1177,9 @@ class AmneziaActivity : QtActivity() {
         return super.dispatchTrackballEvent(ev)
     }
 
+    @Suppress("unused")
+    fun isPlay(): Boolean = BuildConfig.FLAVOR == "play"
+
     /**
      * Utils methods
      */
